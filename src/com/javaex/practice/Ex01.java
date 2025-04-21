275d2ca34b1a7843c43a64f2443cd6526112e6d3
kangsb-010313 <kangsb668@naver.com>
2025-04-21 12:31:28 +0900

    pt02 250421 12:31

---
+++ src/com/javaex/practice/Ex01.java
@@ -0,0 +1,22 @@
+package com.javaex.practice;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		/*
+		 * 정답:1번
+		 * i와 0이 같다는 표현을 할 때에는 == 을 사용해야 함. 
+		다음중 에러가 발생하는 문장은
+		
+		(1)	if( i=0 ) {….}
+		(2)	if( j<10 ) {…}
+		(3)	switch( 'A' ){….}
+		(4)	switch( 100 ){…}
+		(5)	switch( "A" ){…}
+		(6)	if( name == "홍길동" )
+		*/
+		
+		
+	}
+	
+}
